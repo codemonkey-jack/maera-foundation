5ecd1789b9e5c45db00b7dec45006e9eb910240d
Aristeides Stathopoulos <aristath@gmail.com>
2014-12-10 18:12:51 +0200

    combine files

--- maera-foundation.php
+++ maera-foundation.php
@@ -1,4 +1,16 @@
 <?php
+/*
+Plugin Name:         Maera Foundation Shell
+Plugin URI:
+Description:         A shell for the Maera theme implementing Zurb's Foundation framework.
+Version:             0.1-dev
+Author:              @aristath, @fovoc
+Author URI:          http://press.codes
+*/
+
+
+define( 'MAERA_FOUNDATION_SHELL_URL', plugins_url( '', __FILE__ ) );
+define( 'MAERA_FOUNDATION_SHELL_PATH', dirname( __FILE__ ) );
 
 class Maera_ZF {
 
@@ -107,3 +119,20 @@ class Maera_ZF {
     }
 
 }
+
+/**
+* Include the shell
+*/
+function maera_foundation_shell_include( $shells ) {
+
+    // Add our shell to the array of available shells
+    $shells[] = array(
+        'value' => 'foundation',
+        'label' => 'Zurb Foundation',
+        'class' => 'Maera_ZF',
+    );
+
+    return $shells;
+
+}
+add_filter( 'maera/shells/available', 'maera_foundation_shell_include' );
